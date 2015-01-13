e74cc78841ffa280668f281ba36ff2bbd1b4a941
Jussi Vaihia <jussi.vaihia@futurice.com>
2015-01-13 12:18:06 +0200

    add django-dirtyfield dependency

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ class TestCommand(Command):
                              'app_test_runner.py',
                              'test_project']))
 
-install_requires = []
+install_requires = ['django-dirtyfield']
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name = "django-history",
-    version = "0.2.0",
+    version = "0.2.1",
     description = "History for Django ORM data changes",
     url = "http://github.com/futurice/django-history",
     author = "Jussi Vaihia",
