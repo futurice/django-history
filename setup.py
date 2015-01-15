872450346b164b67f75374a6aab4f663bf03788c
Jussi Vaihia <jussi.vaihia@futurice.com>
2015-01-15 16:05:08 +0200

    v0.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name = "django-history",
-    version = "0.2.1",
+    version = "0.2.2",
     description = "History for Django ORM data changes",
     url = "http://github.com/futurice/django-history",
     author = "Jussi Vaihia",
